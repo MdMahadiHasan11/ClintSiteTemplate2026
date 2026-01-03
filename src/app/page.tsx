ef17fcdbe89136f0a9a591c84d2c8406fd3eacbe
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2026-01-03 15:25:27 +0600

    upodate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useAppSelector } from "@/redux/hooks";
+
 export default function Home() {
+  const user = useAppSelector((state) => state.auth.user);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-red-800 dark:text-gray-200">
-        Welcome to Flyghor!
+        Welcome to Flyghor! {user ? `Hello, ${user?.fullName}` : "Guest"}
       </h1>
     </div>
   );
